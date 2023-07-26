3c164a088c3bc77e51924e7d9d2cff2458533168
joychandradas <joychandradas007@gmail.com>
2023-07-27 02:03:40 +0600

    findTwoIndexNumberSum program solved

---
+++ findTwoIndexNumberSum.js
@@ -0,0 +1,20 @@
+const findTwoIndexNumberSum = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    const sum = arr[left] + arr[right];
+    if (sum === target) {
+      return [left, right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [];
+};
+
+const inputArray = [1, 3, 6, 8, 11, 15];
+const targetValue = 16;
+const result = findTwoIndexNumberSum(inputArray, targetValue);
+console.log(result);
